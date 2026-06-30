33c68ec82ef58dda4b4b2fbd65800830d6938098
Suryansh Gupta <suryanshgupta160@gmail.com>
2026-06-30 22:44:12 +0530

    caching added and post controllers

--- post-service/src/controllers/post-controller.js
+++ post-service/src/controllers/post-controller.js
@@ -2,6 +2,14 @@ const Post = require('../models/Post')
 const logger = require('../utils/logger')
 const { validateCreatePost } = require('../utils/validation')
 
+async function invalidatePostCache (req, input) {
+  const keys = await req.redisClient.keys('posts:*')
+
+  if (keys.length > 0) {
+    await req.redisClient.del(keys)
+  }
+}
+
 const createPost = async (req, res) => {
   logger.info('Create post endpoint hit')
 
@@ -25,6 +33,8 @@ const createPost = async (req, res) => {
     })
 
     await newlyCreatedPost.save()
+    await invalidatePostCache(req, newlyCreatedPost._id.toString())
+
     logger.info('Post created successfully', newlyCreatedPost)
     res.status(201).json({
       success: true,
@@ -32,23 +42,79 @@ const createPost = async (req, res) => {
     })
   } catch (e) {
     logger.error('Error creating post', e)
-    message: 'Error creating post'
+    res.status(500).json({ success: false, message: 'Error creating post' })
   }
 }
 
 const getAllPosts = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 10
+    const startIndex = (page - 1) * limit
+
+    const cacheKey = `posts:${page}:${limit}`
+    const cachedPosts = await req.redisClient.get(cacheKey)
+
+    if (cachedPosts) {
+      return res.json(JSON.parse(cachedPosts))
+    }
+
+    const postsQuery = Post.find({})
+      .sort({ createdAt: -1 })
+      .skip(startIndex)
+      .limit(limit)
+
+    const posts = await postsQuery
+    const total = await Post.countDocuments()
+
+    const result = {
+      posts,
+      currentPage: page,
+      totalPages: Math.ceil(total / limit),
+      totalPosts: total
+    }
+
+    // save posts in redis cache
+    await req.redisClient.setex(cacheKey, 300, JSON.stringify(result))
+
+    res.json(result)
   } catch (e) {
     logger.error('Error fetching posts', e)
-    message: 'Error fetching posts'
+    res.status(500).json({ success: false, message: 'Error fetching post' })
   }
 }
 
 const getSinglePost = async (req, res) => {
   try {
+    const postId = req.params.id
+    const cacheKey = `post:${postId}`
+    const cachedPost = await req.redisClient.get(cacheKey)
+
+    if (cachedPost) {
+      return res.json(JSON.parse(cachedPost))
+    }
+
+    const singlePostDetailsById = await Post.findById(postId)
+
+    if (!singlePostDetailsById) {
+      return res.status(404).json({
+        success: false,
+        message: 'Post not found'
+      })
+    }
+
+    await req.redisClient.setex(
+      cacheKey,
+      3600,
+      JSON.stringify(singlePostDetailsById)
+    )
+
+    res.json(singlePostDetailsById)
   } catch (e) {
     logger.error('Error fetching post', e)
-    message: 'Eror fetching post by ID'
+    res
+      .status(500)
+      .json({ success: false, message: 'Error fetching post by id' })
   }
 }
 
--- post-service/src/routes/post-routes.js
+++ post-service/src/routes/post-routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { authenticateRequest } = require('../middlewares/authMiddleware')
-const { createPost } = require('../controllers/post-controller')
+const {
+  createPost,
+  getAllPosts,
+  getSinglePost
+} = require('../controllers/post-controller')
 
 const router = express()
 
 router.use(authenticateRequest)
 
 router.post('/create-post', createPost)
+router.get('/all-posts', getAllPosts)
+router.get('/:id', getSinglePost)
 
 module.exports = router
